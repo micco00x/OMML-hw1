b7ea55a75b12a4dd1e5f797c45646b93b4082ce0
Michele Cipriano <micco00x@gmail.com>
2017-11-25 23:30:59 +0100

    RBFN Extreme Learning implemented

--- src/Functions_homework1_question2_team14.py
+++ src/Functions_homework1_question2_team14.py
@@ -12,3 +12,18 @@ def generate_MLP(X, N, g):
 	G = g(np.matmul(X, W) - b)
 
 	return G, W, b
+
+# Generate matrix G of dimension PxN where X are the sample points,
+# C are the centers, g is the activation function, C has dimension Nxn;
+# G[r][c] = phi(norm(X[r]-C[c])):
+def generate_RBFN(X, C, phi):
+	P = X.shape[0]
+	n = X.shape[1]
+	N = C.shape[0]
+	
+	X = np.reshape(X, (P, 1, n))
+	C = np.reshape(C, (1, N, n))
+	
+	G = phi(np.linalg.norm(X - C, axis=2))
+
+	return G
--- src/Main_homework1_question2_team14.py
+++ src/Main_homework1_question2_team14.py
@@ -1,61 +1,141 @@
 import numpy as np
+import sklearn
+from sklearn import cluster
 import utils
 
-from Functions_homework1_question2_team14 import generate_MLP
+from Functions_homework1_question2_team14 import generate_MLP, generate_RBFN
 
-# hparams:
-N_EXPERIMENTS = 10000
-TEST_SIZE = 0.3
-N = 15
-sigma = 4
-rho = 1e-5
+TEST_MLP = True
+TEST_RBFN = True
 
-# Double check hparams:
-assert TEST_SIZE <= 0.3, "TEST_SIZE must be at most 0.3"
+# Question 2 - Exercise 1: Multi-Layer Perceptron
+if TEST_MLP:
+	print("########################################################################")
+	print("######################## Multi-Layer Perceptron ########################")
+	print("########################################################################")
 
-# Generate dataset:
-X, Y = utils.generate_franke_dataset()
-X_train, Y_train, X_test, Y_test = utils.split_dataset(X, Y, test_size=TEST_SIZE)
+	# hparams:
+	N_EXPERIMENTS = 10000
+	TEST_SIZE = 0.3
+	N = 50
+	sigma = 2
+	rho = 1e-5
 
-# g activation function as specified in Q1E1:
-g = lambda t: (1-np.exp(-sigma*t))/(1+np.exp(-sigma*t))
-P = X_train.shape[0]
+	# Double check hparams:
+	assert TEST_SIZE <= 0.3, "TEST_SIZE must be at most 0.3"
 
-best_training_error = float("inf")
-best_test_error = float("inf")
+	# Generate dataset:
+	X, Y = utils.generate_franke_dataset()
+	X_train, Y_train, X_test, Y_test = utils.split_dataset(X, Y, test_size=TEST_SIZE)
 
-for _ in range(N_EXPERIMENTS):
-	G, W, b = generate_MLP(X_train, N, g)
+	# g activation function as specified in Q1E1:
+	g = lambda t: (1-np.exp(-sigma*t))/(1+np.exp(-sigma*t))
+	P = X_train.shape[0]
 
-	# Solve the linear system for LLSQ problem:
-	v = np.linalg.solve(2*(np.matmul(G.T, G)/(2*P) + rho * np.identity(N)), np.matmul(G.T, Y_train)/P)
+	best_training_error = float("inf")
+	best_test_error = float("inf")
 
-	# f is the MLP as specified in Q1E1:
-	f = lambda x: np.sum(np.multiply(v, g(np.matmul(x, W) - b)), 1)
+	for _ in range(N_EXPERIMENTS):
+		G, W, b = generate_MLP(X_train, N, g)
 
-	# E is the error function:
-	E = lambda x, y: np.mean(np.square(f(x) - y)) / 2 + rho * (np.sum(np.square(W)) + np.sum(np.square(b)) + np.sum(np.square(v)))
+		# Solve the linear system for LLSQ problem:
+		v = np.linalg.solve(2*(np.matmul(G.T, G)/(2*P) + rho * np.identity(N)), np.matmul(G.T, Y_train)/P)
 
-	training_error = E(X_train, Y_train)
-	test_error = E(X_test, Y_test)
+		# f is the MLP as specified in Q1E1:
+		f = lambda x: np.sum(np.multiply(v, g(np.matmul(x, W) - b)), 1)
 
-	# Update best_mlp:
-	if test_error < best_test_error:
-		best_training_error = training_error
-		best_test_error = test_error
-		best_mlp = f
+		# E is the error function:
+		E = lambda x, y: np.mean(np.square(f(x) - y)) / 2 + rho * np.sum(np.square(v))
 
-print("best_training_error:", best_training_error)
-print("best_test_error:", best_test_error)
+		training_error = E(X_train, Y_train)
+		test_error = E(X_test, Y_test)
 
-# Generate data to evaluate, used to plot the approximated function:
-x_range = np.arange(0, 1, 0.01)
-y_range = np.arange(0, 1, 0.01)
-x_grid, y_grid = np.meshgrid(x_range, y_range)
-input_data = []
-for x1, x2 in zip(np.ravel(x_grid), np.ravel(y_grid)):
-	input_data.append([x1, x2])
-input_data = np.array(input_data)
-z_value = np.array(best_mlp(input_data))
-z_grid = np.reshape(z_value, (x_grid.shape[0], x_grid.shape[1]))
-utils.plot_3d(x_grid, y_grid, z_grid, "../images/MLP_Extreme_Learning")
+		# Update best_mlp:
+		if test_error < best_test_error:
+			best_training_error = training_error
+			best_test_error = test_error
+			best_mlp = f
+
+	print("best_training_error:", best_training_error)
+	print("best_test_error:", best_test_error)
+
+	# Generate data to evaluate, used to plot the approximated function:
+	x_range = np.arange(0, 1, 0.01)
+	y_range = np.arange(0, 1, 0.01)
+	x_grid, y_grid = np.meshgrid(x_range, y_range)
+	input_data = []
+	for x1, x2 in zip(np.ravel(x_grid), np.ravel(y_grid)):
+		input_data.append([x1, x2])
+	input_data = np.array(input_data)
+	z_value = np.array(best_mlp(input_data))
+	z_grid = np.reshape(z_value, (x_grid.shape[0], x_grid.shape[1]))
+	utils.plot_3d(x_grid, y_grid, z_grid, "../images/MLP_Extreme_Learning")
+
+# Question 2 - Exercise 2: Radial Basis Function Network
+if TEST_RBFN:
+	print("########################################################################")
+	print("#################### Radial Basis Function Network #####################")
+	print("########################################################################")
+
+	# hparams:
+	N_EXPERIMENTS = 10000
+	TEST_SIZE = 0.3
+	N = 50
+	sigma = 0.5
+	rho = 1e-5
+
+	# Double check hparams:
+	assert TEST_SIZE <= 0.3, "TEST_SIZE must be at most 0.3"
+
+	# Generate dataset:
+	X, Y = utils.generate_franke_dataset()
+	X_train, Y_train, X_test, Y_test = utils.split_dataset(X, Y, test_size=TEST_SIZE)
+
+	# Select the centers randomly (TODO: use clustering):
+	#C = np.take(X_train, np.random.choice(range(X_train.shape[0]), N), axis=0)
+	C = sklearn.cluster.KMeans(n_clusters=N).fit(X_train).cluster_centers_
+
+	# phi activation function as specified in Q1E2:
+	phi = lambda x: np.exp(-np.square(x/sigma)) # be sure that input is of the form norm(x-c)
+	P = X_train.shape[0]
+
+	best_training_error = float("inf")
+	best_test_error = float("inf")
+
+	for _ in range(N_EXPERIMENTS):
+		G = generate_RBFN(X_train, C, phi)
+
+		# Solve the linear system for LLSQ problem:
+		v = np.linalg.solve(2*(np.matmul(G.T, G)/(2*P) + rho * np.identity(N)), np.matmul(G.T, Y_train)/P)
+
+		# f is the RBFN as specified in Q1E2:
+		reshapeX = lambda x: np.reshape(x, (x.shape[0], 1, x.shape[1]))
+		reshapeC = lambda c: np.reshape(c, (1, c.shape[0], c.shape[1]))
+		f = lambda x: np.sum(np.multiply(v, phi(np.linalg.norm(reshapeX(x) - reshapeC(C), axis=2))), 1)
+
+		# E is the error function:
+		E = lambda x, y: np.mean(np.square(f(x) - y)) / 2 + rho * np.sum(np.square(v))
+
+		training_error = E(X_train, Y_train)
+		test_error = E(X_test, Y_test)
+
+		# Update best_rbfn:
+		if test_error < best_test_error:
+			best_training_error = training_error
+			best_test_error = test_error
+			best_rbfn = f
+
+	print("best_training_error:", best_training_error)
+	print("best_test_error:", best_test_error)
+
+	# Generate data to evaluate, used to plot the approximated function:
+	x_range = np.arange(0, 1, 0.01)
+	y_range = np.arange(0, 1, 0.01)
+	x_grid, y_grid = np.meshgrid(x_range, y_range)
+	input_data = []
+	for x1, x2 in zip(np.ravel(x_grid), np.ravel(y_grid)):
+		input_data.append([x1, x2])
+	input_data = np.array(input_data)
+	z_value = np.array(best_rbfn(input_data))
+	z_grid = np.reshape(z_value, (x_grid.shape[0], x_grid.shape[1]))
+	utils.plot_3d(x_grid, y_grid, z_grid, "../images/RBFN_Extreme_Learning")
